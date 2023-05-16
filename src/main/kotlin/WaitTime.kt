027db431752822c0a025b0dfe3758dabade82dad
Bernardo <A50493@alunos.isel.pt>
2023-05-16 20:50:05 +0100

    APP

---
+++ src/main/kotlin/WaitTime.kt
@@ -0,0 +1,15 @@
+
+
+fun waitTimeNano(time: Int) {
+
+    val endtime = System.nanoTime() + time
+    while (System.nanoTime() <= endtime);
+
+}
+
+fun waitTimeMilli(time: Int) {
+
+    val endtime = System.currentTimeMillis() + time
+    while (System.currentTimeMillis() <= endtime);
+
+}
